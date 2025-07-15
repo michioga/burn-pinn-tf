1d300012ac8c138ef13ac55785b4da91238aecc0
Michio Ogawa <michioga@bekkoame.ne.jp>
2025-07-15 21:15:21 +0900

    change cuda as default

--- src/constants.rs
+++ src/constants.rs
@@ -12,16 +12,18 @@ pub mod physics {
     /// ポアソン比。材料が引張られた際の横方向の縮みを示す。今回は未使用。
     pub const POISSON_RATIO: f32 = 0.29; // ポアソン比
 
-    /// 周波数計算式の係数 K。
-    /// プロング（振動する腕）の形状や振動モード、境界条件によって決まる経験的な定数。
-    /// この値はシミュレーションや実験を通じて調整される重要なハイパーパラメータです
-    pub const FREQ_K_CONSTANT: f32 = 0.1615;
+    /// 振動モードの係数
+    pub const K_FACTOR: f32 = 3.5160;
+
+    
 
     /// 損失関数におけるペナルティ項の重み。
     /// `ratio_penalty`（プロング長 > 柄長）に対する重み。
     pub const PENALTY_WEIGHT_RATIO: f32 = 0.5;
     /// `range_penalty`（プロング直径の範囲）に対する重み。
-    pub const PENALTY_WEIGHT_RANGE: f32 = 1.0;
+    pub const PENALTY_WEIGHT_RANGE: f32 = 10.0;
+    /// `range_penalty`（その他の寸法の範囲）に対する重み。
+    pub const PENALTY_WEIGHT_OTHER: f32 = 5.0;
 }
 
 /// モデルの寸法に関する定数
